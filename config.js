ff3416f5aa396564a2cd95591c69536124769475
Atharva Kumar <atharvapraveenkumar@gmail.com>
2025-04-06 14:18:11 +0530

    new

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NODE_ENV: process.env.NODE_ENV,
 	PORT: process.env.PORT || 3000,
-	CORS_ORIGIN: "https://hello.vasanthv.me:*,http://localhost:3000*",
+	CORS_ORIGIN: "https://wequil-meet.vercel.app:*,http://localhost:3000*",
 };
